d2bc1987e9ec59f05a59ee4bc123ffcc5f8d12de
jorjishasan <dev.jorjis@gmail.com>
2024-10-13 06:42:48 +0600

    feat: show Footer only when the showAiComponent is false

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Header from "./components/Header";
 import Body from "./pages/Body";
 import Footer from "./components/Footer";
 import Login from "./pages/Login";
 import Browse from "./pages/Browse";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <>
-        <Header />
-        <Body />
-        <Footer />
-      </>
-    ),
-    children: [
-      { path: "/", element: <Login /> }, // Default route
-      { path: "/browse", element: <Browse /> }, // Browse route
-    ],
-  },
-]);
-
 const App = () => {
+  const showAiSearchComponent = useSelector(
+    (store) => store.aiSearch.showAiSearchComponent,
+  );
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <>
+          <Header />
+          <Body />
+          {!showAiSearchComponent && <Footer />}
+        </>
+      ),
+      children: [
+        { path: "/", element: <Login /> }, // Default route
+        { path: "/browse", element: <Browse /> }, // Browse route
+      ],
+    },
+  ]);
+
   return <RouterProvider router={router} />;
 };
 
